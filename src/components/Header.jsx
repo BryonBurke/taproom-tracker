de088a8bd5775d1e0407cfa6761f1917b1b6ab3d
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-02-14 09:28:06 -0800

    Create header. Start on component structure drawing.

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Header() {
+    const styles = {
+        backgroundColor: '#406aad',
+        fontFamily: 'Roboto',
+        color: 'white',
+        textAlign: 'center',
+        margin: '0',
+        height: '8vh',
+        fontSize: '6vh',
+        lineHeight: '8vh',
+    };
+    return (
+      <div>
+        <h1 style={styles}>Tap Room Tracker</h1>
+      </div>
+    );
+}
+
+export default Header;
