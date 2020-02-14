249756d54c2dcb5535f02730ba6fe55e98ffed4a
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-02-14 14:31:46 -0800

    Basic styling for keglist component

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
-function NewKeg(){
-  return (
-    <div>
-      <form>
-        <input
-          type='text'
-          id='name'
-          placeholder='Keg Name'/>
-        <input
-          type='text'
-          id='Brand'
-          placeholder='Brand'/>
+function KegList(){
 
-        <button type='submit'>Help!</button>
-      </form>
+  const pagegrid = {
+    display:'grid',
+    gridTemplateColumns: '1fr 8fr',
+    gridColumnGap: '10px',
+  }
+
+  const backstyle ={
+    backgroundColor: 'lightblue',
+    height: '500px',
+    padding: '20px'
+  }
+
+
+  return (
+    <div style = {pagegrid}>
+
+      <div >
+        <p style = {backstyle}></p>
+      </div>
+      <div>
+        <p style = {backstyle}></p>
+      </div>
     </div>
-  );
+
+
+);
 }
 
-export default NewKeg;
+export default KegList;
