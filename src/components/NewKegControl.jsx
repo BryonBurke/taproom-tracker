4f8b8ffb9cf536bcdd07a40c734e0ad531e6c942
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-02-21 11:32:39 -0800

    Work on keg control path

--- src/components/NewKegControl.jsx
+++ src/components/NewKegControl.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
-import NewKegForm from './NewKegForm'
 
 
 class NewKegControl extends React.Component {
 
   constructor(props) {
-    super(props);
-    this.state = {
-      formVisibleOnPage: false
-    };
-      this.handleAuthorizedConfirmation = this.handleAuthorizedConfirmation.bind(this);
-  }
+  super(props);
+  this.state = {
+    formVisibleOnPage: false
+  };
+  // this.handleClick = this.handleClick.bind(this);
+}
 
-  handleAuthorizedConfirmation(){
-    this.setState({formVisibleOnPage: true});
-  }
+// handleClick(){
+//   this.setState({formVisibleOnPage: true});
+//   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+// }
 
-  render(){
-    let currentlyVisibleContent = null;
-    if (this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewKegForm />;
-    } else {
-      currentlyVisibleContent = <ConfirmationQuestions onAuthorizedConfirmation={this.handleAuthorizedConfirmation}/>;
-    }
-    return (
-      <ConfirmationQuestions />
-    );
-  }
+render(){
+
+  return (
+    <ConfirmationQuestions />
+  );
+}
 }
 
 export default NewKegControl;
