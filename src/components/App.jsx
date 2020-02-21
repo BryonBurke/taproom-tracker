53d45723b09d18800a648e9ee1c503b6f1616e20
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-02-21 09:31:34 -0800

    Implement NewKegControl routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import KegList from './KegList';
 import NewKeg from './NewKeg';
+import NewKegControl from './NewKegControl';
 import Employee from './Employee';
 import Patron from './Patron';
 import EditKeg from './EditKeg';
@@ -15,7 +16,7 @@ function App(){
     <Header/>
     <Switch>
     <Route exact path='/' component={KegList} />
-    <Route path='/newkeg' component={NewKeg} />
+    <Route path='/newkeg' component={NewKegControl} />
     <Route path='/employee' component={Employee} />
     <Route path='/patron' component={Patron} />
     <Route path='/editkeg' component={EditKeg} />
