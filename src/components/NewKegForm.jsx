af9d500efe9602c016bcff5a410439fb8b13090b
bryon burke <bryonburke@bryons-MBP.hsd1.or.comcast.net>
2020-02-22 14:07:15 -0800

    revert to pre lift state.

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 
 function NewKegForm(){
+
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+  let _alcoholContent = null;
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    _name.value = '';
+    _brand.value = '';
+    _price.value = '';
+    _alcoholContent.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewKegFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Name'/>
+          placeholder='Name'
+          ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='brand'
-          placeholder='Brand'/>
-          <input
-            type='number'
-            id='price'
-            placeholder='Price'/>
-            <input
-              type='text'
-              id='alcoholContent'
-              placeholder='Alcohol Content'/>
+          placeholder='Brand'
+          ref={(input) => {_brand = input;}}/>
+        <input
+          type='number'
+          id='price'
+          placeholder='Price'
+          ref={(input) => {_price = input;}}/>
+        <input
+          type='text'
+          id='alcoholContent'
+          placeholder='Alcohol Content'
+          ref={(input) => {_alcoholContent = input;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
